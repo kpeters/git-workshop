c8381e752cc2d7eebfe2638054371e8cbbe0e3f5
mblaas <52563171+mblaas@users.noreply.github.com>
2019-07-05 11:01:34 +0200

    Update group3.py
    Added print statements

--- Group_assignment/group3.py
+++ Group_assignment/group3.py
@@ -1,3 +1,6 @@
 # print name of 1st groupmember below this line
-
+print('Jeroen')
 # print name of 2nd groupmember below this line
+print('Mike')
+# print name of 3rd groupmember below this line
+print('Michiel')
